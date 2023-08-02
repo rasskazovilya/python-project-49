56b1e7f1f56353c36c45a26f7969e44a0165c4cf
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-02 11:44:54 +0300

    move brain_even to games folder

---
+++ brain_games/scripts/games/brain_even.py
@@ -0,0 +1,31 @@
+import random
+from scripts import printing
+
+
+def is_even(num):
+    return num % 2 == 0
+
+
+def guess():
+
+    for _ in range(3):
+        rand_num = random.randint(1, 1000)
+        printing.ask_question(rand_num)
+
+        answer = input('Your answer: ')
+        correct_answer = 'yes' if is_even(rand_num) else 'no'
+
+        if not printing.is_answer_correct(answer, correct_answer):
+            return False
+
+    return True
+
+
+def main():
+    name = printing.greeting()
+    has_win = guess()
+    printing.print_result(name, has_win)
+
+
+if __name__ == '__main__':
+    main()
