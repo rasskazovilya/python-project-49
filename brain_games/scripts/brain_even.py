0ca8242b4cb10ad089d761f98a86dbc3ef5bd1bb
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-02 11:14:25 +0300

    extract printing logic from guess function to main

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,7 +6,7 @@ def is_even(num):
     return num % 2 == 0
 
 
-def guess(name):
+def guess():
 
     for _ in range(3):
         rand_num = random.randint(1, 1000)
@@ -16,11 +16,9 @@ def guess(name):
         correct_answer = 'yes' if is_even(rand_num) else 'no'
 
         if not is_answer_correct(answer, correct_answer):
-            print('Let\'s try again, {}!'.format(name))
-            return
+            return False
 
-    print('Congratulations, {}!'.format(name))
-    return
+    return True
 
 
 def ask_question(question):
@@ -49,7 +47,12 @@ def main():
     name = cli.welcome_user()
 
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    guess(name)
+    has_win = guess()
+
+    if has_win:
+        print('Congratulations, {}!'.format(name))
+    else:
+        print('Let\'s try again, {}!'.format(name))
 
 
 if __name__ == '__main__':
