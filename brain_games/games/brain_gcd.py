f1491ff42d179f3a78874c193df64b1b34105ff6
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-02 16:07:30 +0300

    add gcd game

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import random
+from brain_games.scripts import logic
+
+
+def gcd(num1, num2):
+    gcd_result = 1
+
+    for i in range(1, min(num1, num2)+1):
+        if (num1 % i == 0) and (num2 % i == 0):
+            gcd_result = i
+    
+    return gcd_result
+
+
+def gcd_game():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+
+    expression = '{} {}'.format(num1, num2)
+    correct_answer = str(gcd(num1, num2))
+
+    return expression, correct_answer
+
+
+def main():
+    logic.engine(gcd_game)
+
+
+if __name__ == '__main__':
+    main()
