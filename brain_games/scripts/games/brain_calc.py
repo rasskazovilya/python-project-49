8a70765b7da36303edbb8cd31db9330edd4f11ec
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-02 11:56:35 +0300

    write new calc game

---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,28 @@
+import random
+from scripts import printing
+
+
+def play_calc_game():
+    
+    operations = ['+', '-', '*']
+
+    for _ in range(3):
+        operation = random.choice(operations)
+        left_num = random.randint(1, 100)
+        right_num = random.randint(1, 100)
+
+        expression = '{} {} {}'.format(left_num, operation, right_num)
+
+        printing.ask_question(expression)
+
+        answer = input('Your answer: ')
+        correct_answer = str(eval(expression))
+
+        if not printing.is_answer_correct(answer, correct_answer):
+            return False
+
+    return True
+
+
+if __name__ == '__main__':
+    printing.main(play_calc_game)
