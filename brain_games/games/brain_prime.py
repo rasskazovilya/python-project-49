1d417e2a2b8b72f19231934e1a811c66481b2769
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-03 10:22:17 +0300

    add corner case for 1 to brain_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,6 +5,9 @@ from brain_games.scripts import logic
 
 def is_prime(num):
     '''Naive implementation of primality test'''
+    if num == 1:
+        return False
+
     for i in range(2, num//2 + 1):
         if num % i == 0:
             return False
