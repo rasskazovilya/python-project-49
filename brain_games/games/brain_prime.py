856577e38a2f563fe40ccc560a76ca1c62fdca4a
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-03 09:24:10 +0300

    add new prime game

---
+++ brain_games/games/brain_prime.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import random
+from brain_games.scripts import logic
+
+
+def is_prime(num):
+    '''Naive implementation of primality test'''
+    for i in range(2, num//2 + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+
+def prime_game():
+    rand_num = random.randint(1, 100)
+    correct_answer = 'yes' if is_prime(rand_num) else 'no'
+
+    return rand_num, correct_answer
+
+
+def main():
+    game_condition = 'Answer "yes" if the number is prime, otherwise answer "no".'
+    logic.engine(prime_game, game_condition)
+
+
+if __name__ == '__main__':
+    main()
