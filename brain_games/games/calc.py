37cea318efc31bc58f43a258a31bd32bbc291217
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-28 12:14:05 +0300

    add constants to replace magic numbers in randint call

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,14 +2,15 @@ import random
 
 
 GAME_RULES = 'What is the result of the expression?'
-
+MIN_NUM = 1
+MAX_NUM = 100
 
 def game():
     operators = ['+', '-', '*']
 
     operator = random.choice(operators)
-    left_num = random.randint(1, 100)
-    right_num = random.randint(1, 100)
+    left_num = random.randint(MIN_NUM, MAX_NUM)
+    right_num = random.randint(MIN_NUM, MAX_NUM)
 
     expression = '{} {} {}'.format(left_num, operator, right_num)
     correct_answer = str(eval(expression))
