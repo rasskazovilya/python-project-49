16cbb4ed002c112cf26e2aa7f67c97ef4f633752
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-28 16:07:37 +0300

    add some constants and fix linter errors

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,16 +2,20 @@ import random
 
 
 GAME_RULES = 'What number is missing in the progression?'
+MAX_INITIAL = 1
 MAX_INITIAL = 50
 MAX_LAST = 200
 MIN_TERMS = 5
+MIN_DIFF = 3
 MAX_DIFF = 10
 
 
-def generate_progression(max_initial_term, max_diff, max_last_term, min_terms):
-    initial = random.randint(1, max_initial_term)
-    last = random.randint(initial + min_terms * max_diff, max_last_term)
-    diff = random.randint(3, max_diff)
+def generate_progression(
+    min_init, max_init, min_diff, max_diff, max_last, min_terms
+):
+    initial = random.randint(min_init, max_init)
+    last = random.randint(initial + min_terms * max_diff, max_last)
+    diff = random.randint(min_diff, max_diff)
     return list(map(str, range(initial, last, diff)))
 
 
@@ -26,7 +30,7 @@ def missing_number(progression, missing):
 
 def play():
     progression = generate_progression(
-        MAX_INITIAL, MAX_DIFF, MAX_LAST, MIN_TERMS
+        MAX_INITIAL, MAX_INITIAL, MIN_DIFF, MAX_DIFF, MAX_LAST, MIN_TERMS
     )
     missing = random.choice(progression)
     progression, correct_answer = missing_number(progression, missing)
