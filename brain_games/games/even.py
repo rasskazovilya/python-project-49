59443babe5e051eba6c293a6cf17f6ed48291bad
Ilya <rasskazov.ilya.98@gmail.com>
2023-08-28 12:15:09 +0300

    add constants to replace magic numbers in randint call in even module

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,14 +2,15 @@ import random
 
 
 GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
-
+MIN_NUM = 1
+MAX_NUM = 1000
 
 def is_even(num):
     return num % 2 == 0
 
 
 def game():
-    rand_num = random.randint(1, 1000)
+    rand_num = random.randint(MIN_NUM, MAX_NUM)
     correct_answer = 'yes' if is_even(rand_num) else 'no'
 
     return rand_num, correct_answer
